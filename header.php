2306b49d03080c1b7c2e676c23e6cd8081ffc915
Bahjat <bahjat.tounsi@le-campus-numerique.Fr>
2022-01-24 15:06:03 +0100

    new header and footer

--- header.php
+++ header.php
@@ -30,3 +30,58 @@
 
 </head>
 
+<body>
+<header>
+    <div>
+        <!-- LOGO -->
+        <a href="index.html" style="height: 100%;"><img alt="Logo JPBC" class="logo"
+                                                        src="public/img/JPBC-logo-white.svg" style="padding: 15px 0 0;"></a>
+        <!-- BTN NAVIGATION DESKTOP-->
+        <nav class="navTop dropdown">
+            <a href="profils.html">Qui sommes nous</a>
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                Réalisations
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="real/cuisine.html">Cuisines</a></li>
+                <li><a class="dropdown-item" href="real/salon.html">Salons</a></li>
+                <li><a class="dropdown-item" href="real/sdb.html">Salles de bain</a></li>
+                <li><a class="dropdown-item" href="real/chambre.html">Chambres</a></li>
+            </ul>
+            <a href="contact.html">Contact</a>
+        </nav>
+        <!-- BTN NAVIGATION RESPONSIVE-->
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="dropdown-item">
+                        <a class="nav-link active" aria-current="page" href="profils.html">Qui sommes nous</a>
+                    </li>
+                    <li class="dropdown-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            Réalisations
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="real/cuisine.html">Cuisines</a></li>
+                            <li><a class="dropdown-item" href="real/salon.html">Salons</a></li>
+                            <li><a class="dropdown-item" href="real/sdb.html">Salles de bain</a></li>
+                            <li><a class="dropdown-item" href="real/chambre.html">Chambres</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown-item">
+                        <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+<main>
+
