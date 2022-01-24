c3712a5c0b80273416eb5b78be490829f0e35b6c
Bahjat <bahjat.tounsi@le-campus-numerique.Fr>
2022-01-24 13:38:25 +0100

    deleted pic folder

--- item.php
+++ item.php
@@ -1,11 +1,11 @@
 <?php
 $nom = "Pixel 6";
 $prix = "650 €";
-$url_photo = "pics/pixel 6.jpg";
+$url_photo = "https://lh3.googleusercontent.com/U4oy7mIHVbKjUOFKnSRndg_oTOP7rLOaiEUo7U6eNEU18hTMlPSqJEGcd9G5YtV2BG27KB7snleH5QZB73J08o8ctjwcYacxGbwT";
 
 echo $nom;
 echo "<br><br>";
 echo $prix;
 echo "<br><br>";
-echo '<a href="pics/pixel 6.jpg'.$url_photo.'"><img src="pics/pixel 6.jpg"></a>';
+echo "<img src=\"$url_photo\" alt=\"pixel 6 picture\">";
 echo "<br><br>";
