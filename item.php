ab569e79221934108d7a121ef3f4f6e1cb41eb88
Bahjat-tounsi <bahjat.tounsi@le-campus-numerique.Fr>
2022-01-24 11:47:25 +0100

    variable

--- item.php
+++ item.php
@@ -1,2 +1,11 @@
 <?php
-echo "hello world";
+$nom = "Pixel 6";
+$prix = "650 €";
+$url_photo = "https://img.bfmtv.com/c/0/0/8b1/cd25eee37881f860bbd6b322f3c0c.jpg";
+
+echo $nom;
+echo "<br>";
+echo $prix;
+echo "<br>";
+echo $url_photo;
+echo "<br>";
