6140fa6c6af2d6d74fe66a03fdea5f46b4f453f4
Bahjat <bahjat.tounsi@le-campus-numerique.Fr>
2022-01-24 13:30:52 +0100

    added pic

--- item.php
+++ item.php
@@ -1,11 +1,11 @@
 <?php
 $nom = "Pixel 6";
 $prix = "650 €";
-$url_photo = "https://img.bfmtv.com/c/0/0/8b1/cd25eee37881f860bbd6b322f3c0c.jpg";
+$url_photo = "pics/pixel 6.jpg";
 
 echo $nom;
-echo "<br>";
+echo "<br><br>";
 echo $prix;
-echo "<br>";
-echo $url_photo;
-echo "<br>";
+echo "<br><br>";
+echo '<a href="pics/pixel 6.jpg'.$url_photo.'"><img src="pics/pixel 6.jpg"></a>';
+echo "<br><br>";
