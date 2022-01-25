2774c9c79a539684ffa48d63fd34a0c1e4dc3091
Bahjat <bahjat.tounsi@le-campus-numerique.Fr>
2022-01-25 14:24:03 +0100

    Sorting arrays

--- simple-catalog.php
+++ simple-catalog.php
@@ -1,18 +1,27 @@
 <?php
 include 'header.php';
 
+
 $products = ["iPhone", "iPad", "iMac"];
+
+print "Tableau fournis:" . "<br>";
+print_r($products);
+
+print "<br><br>";
+
 sort($products);
 
+print "Tableau par ordre alphabetic:" . "<br>";
+print_r($products);
 
+print "<br><br>";
 
-foreach ($products as $val) {
-    echo $val . "\n" . "<br><br>";
-}
+print "Le premier élément du tableau:" . "<br>";
+print_r($products[0]);
 
+print "<br><br>";
 
-
-
-
+print "Le dernier élément du tableau:" . "<br>";
+print_r(end($products));
 
 include 'footer.php';
