b8ff16ed96280641e654b2a8fb45ea16fdd303cd
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-02-22 16:23:48 +0100

    Inheritance beginning

---
+++ CategoryList.php
@@ -0,0 +1,22 @@
+<?php
+
+class CategoryList
+{
+    protected array $categories;
+
+    public function __construct($db)
+    {
+        $categoriesStatement = $db->prepare('SELECT * FROM categories');
+
+        $categoriesStatement->execute();
+        $this->categories = $categoriesStatement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+}
